47f4adc177bea5522590e6652de61b6195a037ac
Harry <harryj.kim1122@gmail.com>
2020-01-05 15:54:54 +0900

    second commit

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
+//Ge bookmarkbar information
+
 function getBBInfo () {
     chrome.bookmarks.getChildren('1', function (bml) {
+        chrome.storage.local.get(['defInfo'], function (reg) {
         var temp = [];
         for (i in bml) {
-            temp[i] = bml[i].url;
+            temp.URLs[i].url = bml[i].url;
+            temp.URLs[i].method = reg.defInfo.DefMethOfBB;
         }
-        result(temp);
         chrome.storage.local.set({bbInfo: temp});
+        });
     });
 }
+
+//Define commands and execute
 
 function setShortcuts () {
     chrome.commands.onCommand.addListener(function (command) {
@@ -19,6 +25,53 @@ function setShortcuts () {
     });
 }
 
+//Open in a new window if so is it set
+
+//!Got a infinite Recursive problem
+//!Cannnot guerantee stability
+
+function watchTabs (tab) {
+    var re = /chrome:\/\/\S*|(?<=\/\/)\S*/;
+    chrome.storage.local.get(['info', 'bbInfo'], function (reg) {
+        console.log('read');
+        if (reg.info.repeated) return;
+        reg.bbInfo.forEach(function (item, _) {
+            if (tab.pendingUrl.match(re)[0] == item.url && item.method == 'newWin') {
+                reg.info.repeated = true;
+                chrome.storage.local.set({info: reg.info}, function () {
+                    chrome.tabs.remove(tab.id);
+                    chrome.windows.create({url: tab.pendingUrl}, function () {
+                        console.log('created');
+                        reg.info.repeated = false;
+                        chrome.storage.local.set({info: reg.info},function (){
+                            console.log('stored');
+                            return;
+                        });
+                    });
+                });
+            }
+        });
+        reg.info.URLs.forEach(function (item, _) {
+            if (tab.pendingUrl.match(re)[0] == item.url && item.method == 'newWin') {
+                reg.info.repeated = true;
+                chrome.storage.local.set({info: reg.info}, function () {
+                    chrome.tabs.remove(tab.id);
+                    chrome.windows.create({url: tab.pendingUrl}, function () {
+                        console.log('created');
+                        reg.info.repeated = false;
+                        chrome.storage.local.set({info: reg.info},function (){
+                            console.log('stored');
+                            return;
+                        });
+                    });
+                });
+            }
+        });
+    });
+}
+
+//Set listeners
+
 /*Uncaught EvalError:
  Refused to evaluate a string as JavaScript
  because 'unsafe-eval' is not an allowed source of script in the following Content
@@ -28,35 +81,57 @@ function setShortcuts () {
 
 function setListeners () {
     chrome.runtime.onInstalled.addListener(getBBInfo);
-    chrome.runtime.onMessage.addListener(clearIf);
+    chrome.runtime.onMessage.addListener(checkMsg);
     chrome.bookmarks.onCreated.addListener(getBBInfo);
     chrome.bookmarks.onRemoved.addListener(getBBInfo);
     chrome.bookmarks.onMoved.addListener(getBBInfo);
     chrome.bookmarks.onChildrenReordered.addListener(getBBInfo);
     chrome.bookmarks.onImportEnded.addListener(getBBInfo);
+    chrome.tabs.onCreated.addListener(watchTabs);
 }
 
+//Define storage if undefined
 
 function initialize () {
-    chrome.storage.local.get(['info'], function (reg) {
+    chrome.storage.local.get(['info', 'defInfo'], function (reg) {
         if (!reg.info) {
-            result(reg);
             clear ();
+        }
+        if (!reg.defInfo) {
+            defClear ();
         }
     });
 }
 
-function clearIf (msg) {
-    if (msg=='clear') {
-        clear();
+//Check message and excute proper function
+
+function checkMsg (msg) {
+    if (msg == 'clear') clear();
+    else if (msg == 'urlAdded') {
+        chrome.storage.local.get(['info'], function (reg) {
+            console.log(reg.info.URLs);
+        });
     }
+    else if (msg == 'defClear') defClear();
 }
 
+//Reset URLs
+
 function clear () {
-    chrome.storage.local.set({info : {index : 0, URLs : []}}, function () {
-        result('clear');
+    chrome.storage.local.set({info : {URLs : [], repeated: false}}, function () {
+        console.log('cleared');
     });
 }
+
+//Reset defalut constant
+
+function defClear () {
+    chrome.storage.local.set({defInfo: {DefMethOfBB: 'newTab'}}, function () {
+        console.log('defalut cleared');
+    });
+}
+
+//Main
 
 initialize ();
 
@@ -73,18 +148,13 @@ setShortcuts ();
 
 
 
-
-function result (out) {
-    if (!out) out = 'successful';
-    console.log(out);
-}
-
+//Simpler version of bookmarkbar shortcuts
 
 function goDirect () {
     chrome.commands.onCommand.addListener(function (command) {
         var index = command[command.length-1];
         chrome.bookmarks.getChildren('1', function (bml) {
-            result(bml);
+            console.log(bml);
             chrome.tabs.create({url: bml[index].url});
         });
     });
--- popup/popupJs.js
+++ popup/popupJs.js
@@ -1,34 +1,50 @@
-window.onload = function () {
-    chrome.tabs.query({active: true}, function (tab) {
-        document.getElementById('textInput').placeholder = tab[0].url;
+//Add Listener every time popup.html pops up
 
-    })
+window.onload = function () {
+    placeURL ();
     document.getElementById('submit').onclick = addURL;
     document.getElementById('reset').onclick = clear;
 }
 
+//Store URL object when Save botton got pressed
+
 function addURL () {
+//Get URL information from Form
+    var re = /chrome:\/\/\S*|\S*[.]\S*/;
     var vUrl = document.getElementById('textInput').value;
-    if (!vUrl) vUrl = document.getElementById('textInput').placeholder;
+    if (vUrl === "" ) {
+        vUrl = document.getElementById('textInput').placeholder;
+        if (vUrl ==="") return;
+    }
+    else if (vUrl.match(re) === null) {
+        alert('Invalid URL');
+        document.getElementById('textInput').value = "";
+        placeURL ();
+        return;
+    }
     var vType = document.getElementById('select').value;
+//Append new URL object to Array
     chrome.storage.local.get(['info'], function (reg) {
-        var index = reg.info.index ++
-        reg.info.URLs[index] = {url: vUrl, type: vType};
+        reg.info.URLs[reg.info.URLs.length] = {url: vUrl, type: vType, method: 'newTab'};
         chrome.storage.local.set({info: reg.info}, function () {
-            result(reg.info);
+            chrome.runtime.sendMessage('urlAdded');
         });
     });
-    url.value = "";
-    type.checked = false;
+    document.getElementById('textInput').placeholder = ""
 }
 
+//Set Placeholder
 
+function placeURL () {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        console.log(tabs);
+        var re = /chrome:\/\/\S*|(?<=\/\/)\S*/;
+        document.getElementById('textInput').placeholder =  tabs[0].url.match(re)[0];
+    });
+}
+
+//Send Clear message to background.js
 
 function clear () {
     chrome.runtime.sendMessage('clear');
-}
-
-function result (out) {
-    if (!out) out = 'successful';
-    console.log(out);
 }
